8c374dca4b8f87957f9701aad529f9bbe6fdd874
Sergi Mayol <sergi2808@gmail.com>
2022-03-26 15:06:45 +0100

    updated

--- SortAlgorithmsJava/src/Main/SortAlgorithms.java
+++ SortAlgorithmsJava/src/Main/SortAlgorithms.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
 import Utilities.LinkedList;
@@ -42,6 +37,7 @@ public class SortAlgorithms {
         l.add(20);
         System.out.println(l.printList());
         l.mergeSort();
+        //l.quickSort();
         System.out.println(l.printList());
     }
 
--- SortAlgorithmsJava/src/Utilities/LinkedList.java
+++ SortAlgorithmsJava/src/Utilities/LinkedList.java
@@ -1,5 +1,7 @@
 package Utilities;
 
+import SortAlgorithmsLinkedList.*;
+
 /**
  *
  * @author Sergi
@@ -17,66 +19,12 @@ public class LinkedList<E extends Comparable<E>> {
         this.first = new Node(element, this.first);
     }
 
-    private Node middle(Node firstNode) {
-        if (firstNode == null) {
-            return firstNode;
-        }
-        Node node = firstNode, aux = firstNode;
-        while (aux.getNext() != null && aux.getNext().getNext() != null) {
-            node = node.getNext();
-            aux = aux.getNext().getNext();
-        }
-        return node;
-    }
-
     public void mergeSort() {
-        this.first = mergeSort(this.first);
+        this.first = new MergeSortLinkedList().mergeSort(this.first);
     }
 
-    private Node mergeSort(Node h) {
-        // Base case : if head is null
-        if (h == null || h.getNext() == null) {
-            return h;
-        }
-        // get the middle of the list
-        Node middle = middle(h);
-        Node nextofmiddle = middle.getNext();
-
-        // set the next of middle node to null
-        middle.setNext(null);
-
-        // Apply mergeSort on left list
-        Node left = mergeSort(h);
-
-        // Apply mergeSort on right list
-        Node right = mergeSort(nextofmiddle);
-
-        // Merge the left and right lists
-        Node sortedlist = merge(left, right);
-        return sortedlist;
-    }
-
-    // S’encarrega de fusionar les dues subllistes ordenades l, r
-    // Retorna el punter a l’inici de la llista fusionada
-    private Node merge(Node l, Node r) {
-        Node result = null;
-        /* Base cases */
-        if (l == null) {
-            return r;
-        }
-        if (r == null) {
-            return l;
-        }
-
-        /* Pick either a or b, and recur */
-        if ((int) l.getItem() <= (int) r.getItem()) {
-            result = l;
-            result.setNext(merge(l.getNext(), r));
-        } else {
-            result = r;
-            result.setNext(merge(l, r.getNext()));
-        }
-        return result;
+    public void quickSort() {
+        new QuickSortLinkedList().sort(this.first, getLastNode());
     }
 
     @Override
@@ -92,6 +40,14 @@ public class LinkedList<E extends Comparable<E>> {
             aux = aux.getNext();
         }
         return list + "null";
+    }
+
+    private Node getLastNode() {
+        Node ult = this.first;
+        while (ult.getNext() != null) {
+            ult = ult.getNext();
+        }
+        return ult;
     }
 
 }
