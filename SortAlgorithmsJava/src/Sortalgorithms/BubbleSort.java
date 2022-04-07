7d76a157a6072bc994ed8f8fe8bff8bf8aa62877
Sergi Mayol <sergi2808@gmail.com>
2022-04-07 12:53:18 +0200

    up

--- SortAlgorithmsJava/src/Sortalgorithms/BubbleSort.java
+++ SortAlgorithmsJava/src/Sortalgorithms/BubbleSort.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -34,40 +33,3 @@ public class BubbleSort implements SortInterface {
     }
 
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Sortalgorithms;
-
-import Utilities.SortInterface;
-import static Sortalgorithms.SortUtils.greater;
-import static Sortalgorithms.SortUtils.swap;
-
-/**
- *
- * @author Sergi
- */
-public class BubbleSort implements SortInterface {
-
-    @Override
-    public <T extends Comparable<T>> T[] sort(T[] array) {
-        for (int i = 1, size = array.length; i < size; ++i) {
-            boolean swapped = false;
-            for (int j = 0; j < size - i; ++j) {
-                if (greater(array[j], array[j + 1])) {
-                    swap(array, j, j + 1);
-                    swapped = true;
-                }
-            }
-            if (!swapped) {
-                break;
-            }
-        }
-        return array;
-    }
-
-}
->>>>>>> 71e86f53e3710ca7b4d24a727c6ba38da134abe6
--- SortAlgorithmsJava/src/Sortalgorithms/RadixSort.java
+++ SortAlgorithmsJava/src/Sortalgorithms/RadixSort.java
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Sortalgorithms;
 
 import java.util.Arrays;
@@ -63,69 +57,3 @@ public class RadixSort {
         }
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Sortalgorithms;
-
-import java.util.Arrays;
-
-/**
- *
- * @author Sergi
- */
-public class RadixSort {
-
-    private static int getMax(int[] arr, int n) {
-        int mx = arr[0];
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > mx) {
-                mx = arr[i];
-            }
-        }
-        return mx;
-    }
-
-    private static void countSort(int[] arr, int n, int exp) {
-        int[] output = new int[n];
-        int i;
-        int[] count = new int[10];
-        Arrays.fill(count, 0);
-
-        for (i = 0; i < n; i++) {
-            count[(arr[i] / exp) % 10]++;
-        }
-
-        for (i = 1; i < 10; i++) {
-            count[i] += count[i - 1];
-        }
-
-        for (i = n - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
-        }
-
-        for (i = 0; i < n; i++) {
-            arr[i] = output[i];
-        }
-    }
-
-    private static void radixsort(int[] arr, int n) {
-
-        int m = getMax(arr, n);
-
-        for (int exp = 1; m / exp > 0; exp *= 10) {
-            countSort(arr, n, exp);
-        }
-    }
-
-    static void print(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
->>>>>>> 71e86f53e3710ca7b4d24a727c6ba38da134abe6
